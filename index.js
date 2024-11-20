d4b0d248f7ac82d733ae54441edd50b215d68d5a
Ehte22 <syedehteshamali22@gmail.com>
2024-11-20 12:02:59 +0530

    socket file updated

--- index.js
+++ index.js
@@ -3,29 +3,32 @@ const mongoose = require("mongoose")
 const cors = require("cors")
 const path = require("path")
 const cookieParser = require("cookie-parser")
-const { app, server } = require("../socket/socket")
-const { userProtected } = require("../middlewares/protected")
+const { app, server } = require("./socket/socket")
+const { userProtected } = require("./middlewares/protected")
 require("dotenv").config({ path: "./.env" })
 
 mongoose.connect(process.env.MONGO_URL)
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://mern-chat-feb5.onrender.com",
     credentials: true
 }))
 
 app.use(express.static(path.join(__dirname, "../dist")))
 app.use(express.static("uploads"))
 app.use(express.static("profile"))
-app.use("/api/auth", require("../routes/auth.routes"))
-app.use("/api/chat", userProtected, require("../routes/chat.route"))
-app.use("/api/user", userProtected, require("../routes/user.route"))
+app.use("/api/auth", require("./routes/auth.routes"))
+app.use("/api/chat", userProtected, require("./routes/chat.route"))
+app.use("/api/user", userProtected, require("./routes/user.route"))
 
 
 
+// app.use("*", (req, res) => {
+//     res.status(404).json({ message: "No resource found" })
+// })
 app.use("*", (req, res) => {
-    res.status(404).json({ message: "No resource found" })
+    res.sendFile(path.join(__dirname, "dist", "index.html"))
 })
 
 app.use((err, req, res, next) => {
--- socket/socket.js
+++ socket/socket.js
@@ -1,11 +1,11 @@
 const express = require("express")
 const http = require("http")
-const { Server } = require("socket.io")
+const Server = require("socket.io")
 require("colors")
 
 const app = express()
 const server = http.createServer(app)
-const io = new Server(server, { cors: { origin: "*" } })
+const io = new Server(server, { cors: { origin: "https://mern-chat-feb5.onrender.com" } })
 
 let ONLINE_USERS = []
 let TYPING = []
